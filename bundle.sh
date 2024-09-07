b25a1029033f6ec4b52b8474de8d757600686b40
Lloyd Everett <lloydeverett@gmail.com>
2024-09-07 12:57:25 +0200

    tweaking bundle script

--- bundle.sh
+++ bundle.sh
@@ -3,10 +3,12 @@
 set -euxo pipefail
 
 rm -rf ./dist
+npm install
 npm run build
 cd dist
 perl -pi -e 's/\/assets\//\//g' index.html
 mv ./assets/* .
 rm -rf ./assets
+cp ../*.png ../*.xml ../*.ico ../*.svg ../*.webmanifest .
 cd ..
 
