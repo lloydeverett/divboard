500f0d88db68d948797dc1217da773d79b72213d
Lloyd Everett <lloydeverett@gmail.com>
2024-09-12 16:24:25 +0200

    inset MacOS traffic lights

--- divboard-desktop/src-tauri/src/main.rs
+++ divboard-desktop/src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{LogicalPosition, Manager};
+use tauri_plugin_trafficlights_positioner::WindowExt;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,7 +12,16 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
+      .setup(move |app| {
+        if let Some(window) = app.get_window("main") {
+          #[cfg(target_os = "macos")]
+          // NOTE: Make sure you only call this ONCE per window.
+          let _ = window.setup_traffic_lights_inset(LogicalPosition::new(20.0, 24.0));
+        };
+  
+        Ok(())
+      })
+      .invoke_handler(tauri::generate_handler![greet])
+      .run(tauri::generate_context!())
+      .expect("Error while running Divboard Desktop. Please report this error to the developer.");
+  }
